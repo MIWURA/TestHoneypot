35a2cce1e3a4d575d33beb16ce5fffe208c681d5
MIWURA <panupoljubchin@gmail.com>
2024-04-17 23:10:22 +0700

    edit insertDB.py

--- func.py
+++ func.py
@@ -93,9 +93,9 @@ def Get_ForDw(sort_by):
    
 def getDB(data):
     data_list = data.split(", ")
-    insertrec = mysql.connection.cursor()
-    sqlqurey="insert into honeypot(type,alert,date,time,ip_attacker,ip_server,protocol,comment) values ('"+data_list[0]+"','"+data_list[1]+"','"+data_list[2]+"','"+data_list[3]+"','"+data_list[4]+"','"+"-"+"','"+data_list[5]+"','"+data_list[6]+"')"
-    insertrec.execute(sqlqurey)
-    print("commit")
-    mysql.connection.cursor().commit()
+    with app.app_context():
+        with mysql.connection.cursor() as cur:
+            cur.execute("INSERT INTO honeypot (type, alert, date, time, ip_attacker, ip_server, protocol, comment) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (data_list[0], data_list[1], data_list[2], data_list[3], data_list[4], "-", data_list[5], data_list[6]))
+            print("commit")
+            mysql.connection.commit()
     #db.close()
--- insertDB.py
+++ insertDB.py
@@ -1,3 +1,24 @@
-from app import respond_to_client
+from func import getDB
+import json
+import time
 
+import time
+
+counter = 0
+def respond_to_client():
+    while True:
+        global counter
+        with open("DBcowrie.txt", "r+") as f:
+            log_lines = f.readlines()
+            f.seek(0)
+            f.truncate()
+            for line in log_lines:
+                getDB(line)  # ลบช่องว่างและตัวขึ้นบรรทัดในท้ายของข้อความ
+                print(line)
+                print("******************")
+                print(counter)
+                counter += 1
+        time.sleep(0.5)
+
+      
 respond_to_client()
