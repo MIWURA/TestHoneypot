03128ba0f8d231c668865597de4466db57deb405
MIWURA <panupoljubchin@gmail.com>
2024-04-18 01:04:30 +0700

    edit logmanagement

--- func.py
+++ func.py
@@ -92,9 +92,9 @@ def Get_ForDw(sort_by):
         return data
    
 def getDB(data):
-    data_list = data.split(", ")
     with app.app_context():
         with mysql.connection.cursor() as cur:
+            data_list = data.split(", ")
             print(data)
             cur.execute("INSERT INTO honeypot (type, alert, date, time, ip_attacker, ip_server, protocol, comment) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (data_list[0], data_list[1], data_list[2], data_list[3], data_list[4], "-", data_list[5], data_list[6]))
             print("commit")
