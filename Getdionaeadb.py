2fb52c3d16fb62344cff55ec781b1211a4e4d5eb
MIWURA <panupoljubchin@gmail.com>
2024-05-06 02:25:36 +0700

    modify dionaealog

--- Getdionaeadb.py
+++ Getdionaeadb.py
@@ -15,34 +15,36 @@ while True:
             time_str = temp[1][:-1]
             if temp[2] == "log_sqlite" :
                 if temp[4]=="accepted" and temp[5]=="connection":
-                    alert = "YELLOW!"
+    
                     ips_temp = temp[9].split(':')
                     ipa_temp = temp[7].split(':')
                     #protocol = ips_temp[1]
                     if ips_temp[1] == "21":
+                        alert = "YELLOW!"
                         protocol = "ftp"
                     elif ips_temp[1] == "42":
+                        alert = "YELLOW!"
                         protocol = "nameserver"
                     elif ips_temp[1] == "443":
+                        alert = "YELLOW!"
                         protocol = "https"
                     elif ips_temp[1] == "80":
+                        alert = "YELLOW!"
                         protocol = "http"
                     elif ips_temp[1] == "5060":
+                        alert = "YELLOW!"
                         protocol = "sip"
                     elif ips_temp[1] == "1433":
+                        alert = "YELLOW!"
                         protocol = "ms-sql-s"
                     elif ips_temp[1] == "1723":
+                        alert = "YELLOW!"
                         protocol = "pptp"
                     elif ips_temp[1] == "9100":
+                        alert = "YELLOW!"
                         protocol = "jetdirect"
                     ip_of_attack = ipa_temp[0]
                     type_of_attack  = "Someone try to connect server and get some data"
-            elif temp[2] == "ftp" :
-                if temp[4]=="cmd" and temp[5]=="'b'USER''":
-                    alert = "RED!"
-                    protocol = "ftp"
-                    ip_of_attack = "192.168.1.106"
-                    type_of_attack  = "Someone try to login server"
 
         if alert != "":
             data_log =  "Dionaea, "+alert+", "+date+", "+time_str+", "+ip_of_attack+", "+protocol+", "+type_of_attack+"\n"
