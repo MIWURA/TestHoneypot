e49a425725a14c60cf141b43c667b1e015857691
MIWURA <panupoljubchin@gmail.com>
2024-05-11 20:26:07 +0700

    cowriepage chang

--- cowriepage.py
+++ cowriepage.py
@@ -1,34 +1,34 @@
 from flask_mysqldb import MySQL
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify,Flask
 from sqlalchemy import func
-from app import db, app
 
+
+app = Flask(__name__)
 cowriepage = Blueprint('cowriepage', __name__)
 
-cowrie_mysql = MySQL()
+cowrie_mysql = MySQL(app)
 
 cowrie_mysql.config['MYSQL_HOST'] = 'localhost'
 cowrie_mysql.config['MYSQL_USER'] = 'mypot'
 cowrie_mysql.config['MYSQL_PASSWORD'] = 'Mypot@123'
 cowrie_mysql.config['MYSQL_DB'] = 'cowrie'
 
-cowrie_mysql.init_app(cowriepage)
 
-class AuthLog(db.Model):
+class AuthLog(cowriepage.Model):
     __tablename__ = 'auth'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    id = cowriepage.Column(cowriepage.Integer, primary_key=True)
+    username = cowriepage.Column(cowriepage.String(100), nullable=False)
+    password = cowriepage.Column(cowriepage.String(100), nullable=False)
 
 @cowriepage.route('/ShowtableCowrie', methods=['POST'])
 def ShowtableCowrie():
     selected_option = request.form['selection']
     if selected_option == 'username':
-        username_count = db.session.query(AuthLog.username, func.count(AuthLog.username)).group_by(AuthLog.username).order_by(func.count(AuthLog.username).desc()).limit(20).all()
+        username_count = AuthLog.query.with_entities(AuthLog.username, func.count(AuthLog.username)).group_by(AuthLog.username).order_by(func.count(AuthLog.username).desc()).limit(20).all()
         result = [{'username': row[0], 'count': row[1]} for row in username_count]
         return jsonify(result)
     elif selected_option == 'password':
-        password_count = db.session.query(AuthLog.password, func.count(AuthLog.password)).group_by(AuthLog.password).order_by(func.count(AuthLog.password).desc()).limit(20).all()
+        password_count = AuthLog.query.with_entities(AuthLog.password, func.count(AuthLog.password)).group_by(AuthLog.password).order_by(func.count(AuthLog.password).desc()).limit(20).all()
         result = [{'password': row[0], 'count': row[1]} for row in password_count]
         return jsonify(result)
     else:
