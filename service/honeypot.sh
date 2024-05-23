dbc8467412ac8c4809a8440b164c504bae713d21
MIWURA <panupoljubchin@gmail.com>
2024-05-24 00:20:47 +0700

    create floder log

--- service/honeypot.sh
+++ service/honeypot.sh
@@ -4,4 +4,4 @@ nohup /usr/bin/python /home/os/TestHoneypot/log/reset.py &
 nohup /usr/bin/python /home/os/TestHoneypot/log/Getcowriedb.py &
 nohup /usr/bin/python /home/os/TestHoneypot/log/Getdionaeadb.py &
 nohup /usr/bin/python /home/os/TestHoneypot/log/insertDB.py &
-nohup /usr/bin/python /home/os/TestHoneypot/app.py &
+
