7ce409a2c866809d1e138e5d81b2ee0c78eadb71
MIWURA <panupoljubchin@gmail.com>
2024-06-09 23:23:14 +0700

    add username path in service

--- service/main.py
+++ service/main.py
@@ -5,33 +5,33 @@ import os
 # ดึงค่า username ของผู้ใช้ปัจจุบัน
 username = os.getlogin()
 
-print(username)
-# # รัน reset.py ก่อน
-# print("Running reset.py...")
-# subprocess.run(["python", f"/home/{username}/TestHoneypot/log/reset.py"])
 
-# # รัน Getcowriedb.py ใน background
-# print("Running backup.py...")
-# subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/backup.py"])
+# รัน reset.py ก่อน
+print("Running reset.py...")
+subprocess.run(["python", f"/home/{username}/TestHoneypot/log/reset.py"])
 
-# # รัน Getcowriedb.py ใน background
-# print("Running Getcowriedb.py...")
-# subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/Getcowriedb.py"])
+# รัน Getcowriedb.py ใน background
+print("Running backup.py...")
+subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/backup.py"])
 
-# # รัน Getdionaeadb.py ใน background
-# print("Running Getdionaeadb.py...")
-# subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/Getdionaeadb.py"])
+# รัน Getcowriedb.py ใน background
+print("Running Getcowriedb.py...")
+subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/Getcowriedb.py"])
 
-# # รัน insertDB.py ใน background
-# print("Running insertDB.py...")
-# subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/insertDB.py"])
+# รัน Getdionaeadb.py ใน background
+print("Running Getdionaeadb.py...")
+subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/Getdionaeadb.py"])
 
-# # รัน Flask application
-# print("Running Flask app.py...")
-# subprocess.Popen(["python", f"/home/{username}/TestHoneypot/app.py"])
+# รัน insertDB.py ใน background
+print("Running insertDB.py...")
+subprocess.Popen(["python", f"/home/{username}/TestHoneypot/log/insertDB.py"])
 
-# print("All scripts started.")
+# รัน Flask application
+print("Running Flask app.py...")
+subprocess.Popen(["python", f"/home/{username}/TestHoneypot/app.py"])
 
-# # Loop to keep the script running
-# while True:
-#     time.sleep(60)
+print("All scripts started.")
+
+# Loop to keep the script running
+while True:
+    time.sleep(60)
