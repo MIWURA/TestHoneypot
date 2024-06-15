7e07c39263b9f68513c00d3b9142ffad9e7d0fe8
MIWURA <panupoljubchin@gmail.com>
2024-06-15 23:18:17 +0700

    Modify wep

--- static/js/app.js
+++ static/js/app.js
@@ -1,49 +1,75 @@
-$(document).ready(function () {
+var socket = io.connect();
 
-    var socket = io.connect();
+///setInterval(function() {
+///    if (socket.connected) {
+///       console.log("Connected to server");
+///    } else {
+///        console.log("Not connected to server");
+///    }
+///}, 3000);
 
-    ///setInterval(function() {
-    ///    if (socket.connected) {
-    ///       console.log("Connected to server");
-    ///    } else {
-    ///        console.log("Not connected to server");
-    ///    }
-    ///}, 3000);
+socket.on("updateResponse_data", function (msg) {
+    //console.log("Response_data :: " + msg);
+    var data = msg;
 
-    socket.on("updateResponse_data", function (msg) {
-        //console.log("Response_data :: " + msg);
-        var data = msg;
+    // ดึง tbody ของตาราง
+    var tbody = document.getElementById("data-table-body");
 
-        // ดึง tbody ของตาราง
-        var tbody = document.getElementById("data-table-body");
+    // ลบข้อมูลเก่าทั้งหมดใน tbody   
+    tbody.innerHTML = "";
 
-        // ลบข้อมูลเก่าทั้งหมดใน tbody   
-        tbody.innerHTML = "";
+    // เพิ่มข้อมูลใหม่ลงใน tbody
+    data.data.forEach(function (rowData) {
+        var row = document.createElement("tr");
 
-        // เพิ่มข้อมูลใหม่ลงใน tbody
-        data.data.forEach(function (rowData) {
-            var row = document.createElement("tr");
+        Object.values(rowData).forEach(function (cellData) {
+            var cell = document.createElement("td");
 
-            Object.values(rowData).forEach(function (cellData) {
-                var cell = document.createElement("td");
-
-                // ตรวจสอบเงื่อนไขและเพิ่มคลาส CSS ตามเงื่อนไข
-                if (cellData === "RED!") {
-                    cell.classList.add("red-cell");
-                } else if (cellData === "YELLOW!") {
-                    cell.classList.add("yellow-cell");
-                } else if (cellData === "ORANGE!") {
-                    cell.classList.add("orange-cell");
-                }
-
+            // ตรวจสอบเงื่อนไขและเพิ่มวงกลมตามเงื่อนไข
+            if (cellData === "RED!") {
+                var circle = document.createElement("span");
+                circle.classList.add("circle", "red-circle");
+                cell.appendChild(circle);
+            } else if (cellData === "YELLOW!") {
+                var circle = document.createElement("span");
+                circle.classList.add("circle", "yellow-circle");
+                cell.appendChild(circle);
+            } else if (cellData === "ORANGE!") {
+                var circle = document.createElement("span");
+                circle.classList.add("circle", "orange-circle");
+                cell.appendChild(circle);
+            } else {
                 cell.textContent = cellData;
-                row.appendChild(cell);
-            });
+            }
 
-            tbody.appendChild(row);
+            row.appendChild(cell);
         });
+
+        tbody.appendChild(row);
     });
-
-
 });
 
+// CSS
+var style = document.createElement('style');
+style.innerHTML = `
+    .circle {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        margin-right: 5px;
+    }
+
+    .red-circle {
+        background-color: red;
+    }
+
+    .yellow-circle {
+        background-color: yellow;
+    }
+
+    .orange-circle {
+        background-color: orange;
+    }
+`;
+document.head.appendChild(style);
